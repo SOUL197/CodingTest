0b2527df25fbb01653423c42c34b15899ffc323d
SOUL197 <ssm970@naver.com>
2026-03-06 14:10:07 +0900

    [level 1] Title: 숫자 짝꿍, Time: 92.12 ms, Memory: 160 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String X, String Y) {
+        char[] charX = X.toCharArray();
+        char[] charY = Y.toCharArray();
+        Arrays.sort(charX);
+        Arrays.sort(charY);
+
+        StringBuilder answerBuild = new StringBuilder();
+
+        int idxX = charX.length - 1;
+        int idxY = charY.length - 1;
+
+        while (idxX >= 0 && idxY >= 0) {
+            if (charX[idxX] == charY[idxY]) {
+                answerBuild.append(charX[idxX]);
+                idxX--;
+                idxY--;
+            } else if (charX[idxX] > charY[idxY]) {
+                idxX--;
+            } else {
+                idxY--;
+            }
+        }
+
+        if (answerBuild.length() == 0) {
+            return "-1";
+        }
+        if (answerBuild.charAt(0) == '0') {
+            return "0";
+        }
+
+        return answerBuild.toString();
+    }
+}
