f93204bd56694cc7808d3855933ce949e65c1842
SOUL197 <ssm970@naver.com>
2026-03-16 19:24:49 +0900

    [level 1] Title: 추억 점수, Time: 2.41 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+class Solution {
+    public ArrayList solution(String[] name, int[] yearning, String[][] photo) {
+        ArrayList<Number> answer = new ArrayList<>();
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i=0; i<name.length; i++){
+            map.put(name[i],yearning[i]);
+        }
+        
+        for (String[] p : photo){
+            int a = 0;
+            for (String n : p){
+                if (map.containsKey(n)){
+                    a += map.get(n);
+                }
+            }
+            answer.add(a);
+        }
+        
+        // System.out.println(map.containsKey("may"));
+        return answer;
+    }
+}
